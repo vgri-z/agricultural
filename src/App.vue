00425ca54aabaa9b7d4c31e8cca8972e02d46b84
vgri <vgri199712@163.com>
2024-04-19 16:25:13 +0800

    项目初始化

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="app"></div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+.app {
+  height: 100vh;
+  width: 100vw;
+  overflow: hidden;
+}
+</style>
---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+import 'normalize.css'
+import './assets/css/index.less'
+
+const app = createApp(App)
+app.mount('#app')
